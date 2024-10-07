4790643402a034d8ae24b6ef4f51f8407ffcbf0e
shreej2302 <shreej2302@gmail.com>
2024-10-07 14:56:21 +0530

    More Variables and Printing

---
+++ exercises/ex5.py
@@ -0,0 +1,15 @@
+name = 'Zed Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print("Let's talk about {}.".format(name))
+print("He's {} inches tall.".format(height))
+print("Actually that's not too heavy.")
+print("He's got {} eyes and {} hair.".format(eyes, hair))
+print("His teeth are usually {} depending on the coffee.".format(teeth))
+
+print("If I had {}, {}, and {} I get {}.".format(age, height, weight, age + height + weight))
