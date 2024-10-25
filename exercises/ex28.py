470ffde7bb4731c8a6afad1e3993d11c305848b5
shreej2302 <163439838+shreej2302@users.noreply.github.com>
2024-10-25 20:49:41 +0530

    Loops and Lists

--- exercises/ex28.py
+++ exercises/ex28.py
@@ -3,7 +3,6 @@ fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 #this kind of for-loop goes through a list
-
 for number in the_count:
   print("This is count " + str(number))
 
